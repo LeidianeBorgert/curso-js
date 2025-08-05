8c1336a1a98821ceefa36394d507ff78eb88ebce
leidiane <leidianeborgert@gmail.com>
2025-08-04 21:52:34 -0300

    adicionado exercicios

---
+++ js/exemplo01-conversoes.js
@@ -0,0 +1,13 @@
+//string
+function converterStringParaInt(){
+    const anoFabricacaoEntradaUsuario = "2025"; //string
+    const anoFabricacao = parseInt(anoFabricacaoEntradaUsuario);
+    alert("Ano Fabricação: " + anoFabricacao);
+}
+function exemploConverterStringParaFloat(){
+    const precoDoCarro = "299800.10";
+    const preco = parseFloat(precoDoCarro);
+    alert("Preço: " + preco);
+}
+//chama a variavel assim q atulizar pagina.
+exemploConverterStringParaFloat(); 
